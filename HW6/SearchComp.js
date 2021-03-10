8f87d28148e5bb56612c7c64d74ec49e803b1a7b
IlyaSaveliev <73860139+IlyaSaveliev@users.noreply.github.com>
2021-03-10 10:14:16 +0300

    Update SearchComp.js

--- HW6/SearchComp.js
+++ HW6/SearchComp.js
@@ -1,19 +1,21 @@
-Vue.component('search', {
-    data() {
-        return {
-        };
-    },
 
-    mounted() {
-        console.log('search');
-    },
+// Этот вариант работать отказался.
+// Vue.component('search', {
+//     data() {
+//         return {
+//         };
+//     },
 
-    template: `
-    <form action="#" class="search-form" @submit.prevent="filter">
-    <input type="text" class="search-field" v-model="userSearch">
-    <button class="btn-search" @click="$emit(filter)" type="submit">
-    <i class="fas fa-search"></i>
-    </button>
-    </form>`
+//     mounted() {
+//         console.log('search');
+//     },
 
-});
+//     template: `
+//     <form action="#" class="search-form" @submit.prevent="filter">
+//     <input type="text" class="search-field" v-model="userSearch">
+//     <button class="btn-search" @click="$emit(filter)" type="submit">
+//     <i class="fas fa-search"></i>
+//     </button>
+//     </form>`
+
+// });
